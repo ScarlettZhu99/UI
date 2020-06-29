911fdff5bd84629b2bfe8f3ef3f6c2914dffca96
Scarlett Zhu <mia.zhp@qq.com>
2020-06-29 14:42:27 -0400

    day09

---
+++ log.js
@@ -0,0 +1,17 @@
+// var log = {
+//     info: function(info) {
+//         console.log("info: " + info)
+//     },
+//     warning: function(warning) {
+//         console.log("info: " + warning)
+//     },
+//     error: function(error) {
+//         console.log("info: " + error)
+//     }
+// }
+
+// module.exports = log
+
+module.exports = "Some string "
+
+exports.name = "name"
---
+++ test.js
@@ -0,0 +1,24 @@
+//  var mylog = require('./log')
+// mylog.error("some error")
+
+//  console.log(mylog)
+
+//  console.log(mylog.name)
+
+// var fs = require('fs')
+
+// //async
+// fs.readFile('data.txt', function(err, data) {
+//     if (err) {
+//         return console.log(err);
+
+//     } else {
+//         console.log(data.toString())
+//     }
+
+// })
+////////////
+
+// var fs = require('fs')
+// var data = fs.readFileSync('data.txt');
+// console.log(data.toString())
